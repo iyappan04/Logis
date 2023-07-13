5b71f2c55d64f78f3478cf9262d6e24a80221040
iyappan04 <iyappank165@gmail.com>
2023-07-13 18:21:36 +0530

    Provide Section Completed.

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '769px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      gradientColorStops: {
+        'callgradient': 'rgba(14, 29, 52, 0.6)',
+      },
+      backgroundImage: {
+        'heroBg': "url('/img/hero-bg.png')",
+        'callBg': "url('/img/cta-bg.jpg')",
+      },
+      colors: {
+        Default: "#0a0d13",
+        Primary: "#0d42ff",
+        Secondary: "#0e1d34",
+        White: "#fff",
+        Blue: "#001f8d",
+        callBgColor: "linear-gradient(rgba(14, 29, 52, 0.6),rgba(14, 29, 52, 0.6))"
+      },
+    },
+  },
+  plugins: [],
+}
+
